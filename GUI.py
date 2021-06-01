80ea6dbe3d4e212966681970ff5a5f10341797ed
pwzhang21 <pwzhang@crimson.ua.edu>
2021-06-01 04:47:59 -0500

    added basic functionality for loading dms

--- GUI.py
+++ GUI.py
@@ -8,18 +8,23 @@ import requests
 import webbrowser
 
 
-# return a render of an image from a url
-def get_img_from_url(url):
+def config_frame(canvas):
+    canvas.configure(scrollregion=canvas.bbox(tk.ALL))
+
+
+# return a render of an image from a url TODO: find way to improve performance
+def get_img_from_url(url, x, y):
     resp = requests.get(url)
     img = Image.open(BytesIO(resp.content))
-    img = img.resize((100, 100), Image.ANTIALIAS)  # TODO: find good size for images
+    img = img.resize((x, y), Image.ANTIALIAS)
     render = ImageTk.PhotoImage(img)
     return render
 
 
 class GUI:
-    main_frame = None
-    root = None
+    client: Client = None
+    main_frame: tk.Frame = None
+    root: tk.Tk = None
 
     def __init__(self):
         self.root = tk.Tk()
@@ -30,60 +35,62 @@ class GUI:
         if self.main_frame is not None:
             self.main_frame.destroy()
 
+    def setup_window(self, title, dimensions, x_resize, y_resize):
+        self.root.title(title)
+        self.root.geometry(dimensions)
+        self.root.resizable(x_resize, y_resize)
+        main_menu_frame = tk.Frame(
+            self.root
+        )
+        main_menu_frame.pack(fill=tk.BOTH, expand=True)
+        self.main_frame = main_menu_frame
+
+    # TODO: figure out better size for window, improve handling for invalid tokens
     def setup_login(self):
         self.clear_main()
-        self.root.title("GroupMeUtils")
-        self.root.geometry("250x100")
-        self.root.resizable(0, 0)
-        login_frame = tk.Frame(
-            self.root,
-        )
-        login_frame.pack()
-        self.main_frame = login_frame
+        self.setup_window('GroupMeUtils', '250x100', False, False)
         api_key_lbl = tk.Label(
-            login_frame,
-            text="Enter Access Token:"
+            self.main_frame,
+            text='Enter Access Token:'
         )
         api_key_lbl.pack()
         api_key_ent = tk.Entry(
-            login_frame,
+            self.main_frame,
             width=25
         )
         api_key_ent.pack()
         go_btn = tk.Button(
-            login_frame, text="Go",
+            self.main_frame, text='Go',
             width=10,
             height=1
         )
+        go_btn.bind('<Button-1>', lambda e: self.login(api_key_ent.get()))
         go_btn.pack()
-        go_btn.bind("<Button-1>", lambda e: self.login(api_key_ent.get()))
         get_key_lbl = tk.Label(
-            login_frame,
-            text="Click here to get your Access Token"
+            self.main_frame,
+            text='Click here to get your Access Token'
         )
+        get_key_lbl.bind('<Button-1>', lambda e: webbrowser.open('https://dev.groupme.com/session/new'))
         get_key_lbl.pack()
-        get_key_lbl.bind("<Button-1>", lambda e: webbrowser.open("https://dev.groupme.com/session/new"))
 
     def login(self, access_key):
         client = Client.from_token(access_key)
         try:
             client.user.get_me()
-            self.setup_main_menu(client)
+            self.client = client
+            self.setup_main_menu()
         except exceptions.BadResponse:
             # tell user their access token is incorrect
-            print("Bad Token")
+            print('Bad Token')
 
-    def setup_main_menu(self, client):
-        self.root.geometry("300x100")
-        user_info = client.user.get_me()
+    # TODO: make information copyable, make buttons work, make better loading
+    def setup_main_menu(self):
         self.clear_main()
-        main_menu_frame = tk.Frame(
-            self.root,
-        )
-        main_menu_frame.pack()
-        self.main_frame = main_menu_frame
+        self.setup_window('GroupMeUtils', '300x175', False, False)
+        user_info = self.client.user.get_me()
+
         profile_info_lbl = tk.Label(
-            main_menu_frame,
+            self.main_frame,
             text=f'Id: {user_info["id"]}\n'
                  f'Name: {user_info["name"]}\n'
                  f'Email: {user_info["email"]}\n'
@@ -91,10 +98,60 @@ class GUI:
                  f'Creation Date: {datetime.fromtimestamp(user_info["created_at"])}'
         )
         profile_info_lbl.grid(row=0, column=0)
-        prof_render = get_img_from_url(client.user.get_me()['image_url'])
+        prof_render = get_img_from_url(user_info['image_url'], 100, 100)
         profile_pic = tk.Label(
-            main_menu_frame,
+            self.main_frame,
             image=prof_render
         )
         profile_pic.image = prof_render
         profile_pic.grid(row=0, column=1)
+
+        load_dms_btn = tk.Button(
+            self.main_frame,
+            text='Load DMs'
+        )
+        load_dms_btn.bind('<Button-1>', lambda e: self.setup_dms_menu())
+        load_dms_btn.grid(row=1, column=0)
+
+        load_groups_btn = tk.Button(
+            self.main_frame,
+            text='Load Groups'
+        )
+        load_groups_btn.grid(row=2, column=0)
+
+        save_pfp_btn = tk.Button(
+            self.main_frame,
+            text='Save Picture'
+        )
+        save_pfp_btn.grid(row=1, column=1)
+
+    def setup_dms_menu(self):
+        self.clear_main()
+        self.setup_window('GroupMeUtils', '300x600', False, False)
+        dm_canvas = tk.Canvas(
+            self.main_frame,
+            borderwidth=0
+        )
+        dm_frame = tk.Frame(
+            dm_canvas
+        )
+        vsb = tk.Scrollbar(self.main_frame, orient=tk.VERTICAL, command=dm_canvas.yview)
+        dm_canvas.configure(yscrollcommand=vsb.set)
+        vsb.pack(side=tk.RIGHT, fill=tk.Y)
+        dm_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        dm_canvas.create_window((0, 0), window=dm_frame, anchor=tk.NW)
+        dm_frame.bind('<Configure>', lambda e: config_frame(dm_canvas))
+        self.load_dms(dm_frame)
+
+    #TODO: picture for no avatar
+    def load_dms(self, frame):
+        row = 0
+        for chat in self.client.chats.list_all():
+            if chat.other_user['avatar_url'] != '':
+                pfp_render = get_img_from_url(chat.other_user['avatar_url'], 50, 50)
+                pfp = tk.Label(frame, image=pfp_render)
+                pfp.image = pfp_render
+                pfp.grid(row=row, column=0)
+            tk.Label(frame, text=chat.other_user['name']).grid(row=row, column=1)
+            row += 1
+
